083765161764c3d4f0580ef267293e0b66f95211
Mayank Kumar <developer-at-work@Mayanks-MacBook-Air.local>
2025-06-09 10:08:20 +0530

    added changes for conflicts

--- components/Screens/SplashScreens/Splashone.js
+++ components/Screens/SplashScreens/Splashone.js
@@ -27,11 +27,22 @@ const Splashone = () => {
   }, []);
 
   return (
-    <>
+    <ImageBackground
+      style={[
+        styles.container,
+        {
+          justifyContent: "space-between",
+          backgroundColor: "white",
+        },
+      ]}
+      source={require("../../../assets/images/background/back.png")}
+    >
       <SafeAreaView style={styles.container}>
         <StatusBar barStyle="dark-content" backgroundColor="white" />
+
         <View style={{ marginHorizontal: 50 }}>
           <CustomLogo
+            color={"white"}
             image={require("../../../assets/images/logo_comp/nj_house_map.png")}
           />
           <View style={{ justifyContent: "center", alignItems: "center" }}>
@@ -63,7 +74,8 @@ const Splashone = () => {
                 styles.text,
                 {
                   fontSize: 15,
-                  color: "gray",
+                  color: "white",
+
                   marginTop: 30,
                   fontWeight: "200",
                   textAlign: "center",
@@ -77,16 +89,18 @@ const Splashone = () => {
         </View>
       </SafeAreaView>
       <View
-        style={{
-          backgroundColor: "white",
-        }}
+        style={
+          {
+            // backgroundColor: "white",
+          }
+        }
       >
         <Botttombar
           title="Get Started"
           navigation={() => navigation.navigate("Splashtwo")}
         />
       </View>
-    </>
+    </ImageBackground>
   );
 };
 
@@ -95,20 +109,19 @@ export default Splashone;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "white",
+    paddingTop: 100,
   },
   content: {
     justifyContent: "center",
     width: "100%",
-    height: 600,
-    // alignItems: "center",
+    top: 130,
+
     position: "absolute",
   },
   text: {
     fontSize: 12,
     fontWeight: "500",
-    color: "black",
+    color: "white",
   },
 });
